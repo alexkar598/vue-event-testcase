a33e4ca4b8a58eb6e669b1dbe7407976a6492dd2
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2023-03-21 14:41:16 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import MyComponent from "./MyComponent";
+
+function handleEvent(message: string) {
+  alert(message)
+}
+</script>
+
+<template>
+  <div>
+    <MyComponent @custom="handleEvent" message="Hello World!"></MyComponent>
+  </div>
+</template>
---
+++ src/original/MyComponent.ts
@@ -0,0 +1,22 @@
+import {defineComponent, h} from "vue";
+
+export default defineComponent({
+  name: "MyComponent",
+  props: {
+    message: { type: String, required: true}
+  },
+  emits: {
+    custom(message: string) {
+      return true
+    }
+  },
+  setup(props, ctx) {
+    return () => {
+      return h("button", {
+        onClick() {
+          ctx.emit("custom", props.message)
+        }
+      }, props.message)
+    }
+  }
+})
